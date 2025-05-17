c77e0cab4c4b40f9f6cf23551663411fa5caea0f
Leonardo Melo <leoomelo@gmail.com>
2025-05-17 18:15:00 -0300

    fix: specs adjustments

--- spec/services/file_service/process_spec.rb
+++ spec/services/file_service/process_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe FileService::Process, type: :service do
   let(:file) { StringIO.new(valid_file_content) }
 
   describe '#call' do
-    it 'processa corretamente o arquivo e executa os comandos' do
+    it 'should process the file and commands' do
       service = described_class.new(file)
       expect(service.call).to be true
 
@@ -22,19 +22,19 @@ RSpec.describe FileService::Process, type: :service do
       expect(service.plateau.height).to eq(5)
       expect(service.results.length).to eq(2)
 
-      expect(service.results[0][:final_position]).to eq("1 3 N")
-      expect(service.results[1][:final_position]).to eq("5 1 E")
+      expect(service.results[0]).to eq("1 3 N")
+      expect(service.results[1]).to eq("5 1 E")
     end
 
-    it 'retorna erro se o arquivo estiver vazio' do
+    it 'should return an error if the file is empty' do
       empty_file = StringIO.new("")
       service = described_class.new(empty_file)
 
       expect(service.call).to be false
-      expect(service.error).to match(/Arquivo vazio/)
+      expect(service.error).to match(/Empty file or bad formmated/)
     end
 
-    it 'retorna erro se faltar linha de comandos' do
+    it 'should return an error if the file is bad formatted' do
       invalid_content = <<~TXT
         5 5
         1 2 N
@@ -42,7 +42,7 @@ RSpec.describe FileService::Process, type: :service do
       service = described_class.new(StringIO.new(invalid_content))
 
       expect(service.call).to be false
-      expect(service.error).to match(/Par de linhas inválido/)
+      expect(service.error).to match(/Empty file or bad formmated/)
     end
   end
 end
