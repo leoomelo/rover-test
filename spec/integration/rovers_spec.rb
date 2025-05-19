38920f3d17f254175a8ca345e49736faeae84fe7
Leonardo Melo <leoomelo@gmail.com>
2025-05-19 09:39:57 -0300

    feat: adding documentation for commands endpoint

--- spec/integration/rovers_spec.rb
+++ spec/integration/rovers_spec.rb
@@ -34,4 +34,49 @@ RSpec.describe 'Rovers API', type: :request do
       end
     end
   end
+
+  path '/api/v1/plateaus/{plateau_id}/rovers/{id}/commands' do
+    post('send commands to rover') do
+      tags 'Rovers'
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :plateau_id, in: :path, type: :integer, description: 'Plateau ID'
+      parameter name: :id, in: :path, type: :integer, description: 'Rover ID'
+
+      parameter name: :command, in: :body, schema: {
+        type: :object,
+        properties: {
+          commands: { type: :string, example: 'MMRMMRMRRM'}
+        },
+        required: ['commands']
+      }
+
+      response(200, 'commands executed successfully') do
+        let!(:plateau) { Plateau.create!(width: 5, height: 5) }
+        let!(:rover) do
+          Rover.create!(
+            plateau: plateau,
+            pos_x: 1,
+            pos_y: 2,
+            direction: 'N'
+          )
+        end
+
+        let(:plateau_id) { plateau.id }
+        let(:id) { rover.id }
+        let(:command) { { commands: 'MMRMMRMRRM' } }
+
+        run_test!
+      end
+
+      response(422, 'invalid commands') do
+        let(:plateau_id) { 1 }
+        let(:id) { 1 }
+        let(:command) { { commands: 'INVALID' } }
+
+        run_test!
+      end
+    end
+  end
 end
